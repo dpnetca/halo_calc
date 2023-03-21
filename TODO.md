7c87d3c069b688c6b06484fe0d3de5cedd9b90f2
dpnetca <git@dpnet.ca>
2023-03-21 17:16:31 -0600

    added handling for when wrong angle is calculated, also handle for path crossing halo twice

--- TODO.md
+++ TODO.md
@@ -2,7 +2,6 @@
 
 - change return to provide more information (currently on provides int as centre of return should include start and end of band as well)
 - add error handling if target system name does not exist
-- add handling if path intesects band twice
-  - python src/main.py arcl1 61333041 47755719 5 (MICl2 -> arcl1 should cross twice, current response gives the second crossing)
 - fill in data for "0" value distances
 - currently catching errors if path is outside halo with a try/except, would like a better way to do this...
+- class needs refactoring it's now a mess
--- src/halo.py
+++ src/halo.py
@@ -3,6 +3,7 @@ from typing import Tuple
 from data import bands, distance_to_centre
 
 
+# clean this up it is now a mess
 class HaloDistance:
     def __init__(self, distance_to_marker: int):
         self.distance_to_marker = distance_to_marker
@@ -11,19 +12,40 @@ class HaloDistance:
         if band == 0:
             self.to_bands(target, distance_to_target)
             return
-        centre = self._to_mid_band(target, distance_to_target, band)
+        centre, centre2 = self._to_mid_band(target, distance_to_target, band)
 
         # If the distance to the the the intersect point with centre of band
         # is greater then the distance to the target then path is fully inside
-        # halo and never intersects, if centre is 0, then the path is outside
-        # the halo and never intersects
-        if centre > distance_to_target or centre == 0:
-            print(f"Selected path does not intersect halo band {band}")
+        # halo and never intersects,
+        if centre > distance_to_target and centre2 > distance_to_target:
+            print(
+                f"Selected path does not intersect halo band {band}, "
+                "start and end point are inside the band"
+            )
             return
-        start, end = self._to_band_edges(target, distance_to_target, band)
+        # if centre is 0, the path is outside the halo and never intersects
+        elif centre == 0:
+            print(
+                f"Selected path does not intersect halo band {band}, "
+                "path is outside the band"
+            )
+            return
 
-        print(f"stop between {int(start):,}km and {int(end):,}km from target")
-        print(f"stop at centre {int(centre):,}km from target")
+        start, end, start2, end2 = self._to_band_edges(
+            target, distance_to_target, band
+        )
+
+        if centre < distance_to_target:
+            print(
+                f"stop between {int(start):,}km and {int(end):,}km from target"
+            )
+            print(f"stop at centre {int(centre):,}km from target")
+        if centre2 < distance_to_target:
+            print(
+                f"stop between {int(start2):,}km and {int(end2):,}km"
+                " from target"
+            )
+            print(f"stop at centre {int(centre2):,}km from target")
 
     def to_bands(self, target: str, distance_to_target: int):
         for band in range(1, 11):
@@ -45,17 +67,17 @@ class HaloDistance:
     ) -> Tuple[int, int]:
         band_start = bands[band]["centre"] - bands[band]["width"] / 2
         band_end = bands[band]["centre"] + bands[band]["width"] / 2
-        start = self._calculate_interesect(
+        start, start2 = self._calculate_interesect(
             distance_to_centre[target],
             distance_to_target,
             band_start,
         )
-        end = self._calculate_interesect(
+        end, end2 = self._calculate_interesect(
             distance_to_centre[target],
             distance_to_target,
             band_end,
         )
-        return (start, end)
+        return (start, end, start2, end2)
 
     def _calculate_interesect(self, centre, target, band):
         try:
@@ -66,7 +88,11 @@ class HaloDistance:
             angle_b = math.asin(centre * math.sin(angle_a) / band)
             angle_c = math.pi - angle_a - angle_b
             band_intersect = (band / math.sin(angle_a)) * math.sin(angle_c)
-        except ValueError:  # Math Domain Error
-            return 0
 
-        return band_intersect
+            angle_b2 = math.pi - angle_b
+            angle_c2 = math.pi - angle_a - angle_b2
+            band_intersect2 = (band / math.sin(angle_a)) * math.sin(angle_c2)
+        except ValueError:  # Math Domain Error
+            return (0, 0)
+
+        return (band_intersect, band_intersect2)
